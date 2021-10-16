212d2821a5b267dd67989f2997cfb59de61def5f
Christian Fritz <christian.fritz@dreamix.eu>
2021-10-16 17:26:37 +0300

    internationalization with ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeBg from '@angular/common/locales/bg';
+import localeEn from '@angular/common/locales/en';
+
+import { TranslateService } from '@ngx-translate/core';
+
+
 
 
 @Component({
@@ -8,4 +15,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  public selectedLanguage: string = 'en';
+  public languages = ['bg', 'en']; 
+
+  constructor(public translate: TranslateService) { 
+    registerLocaleData(localeBg, 'bg');
+    registerLocaleData(localeEn, 'en');
+  }
+
+  public changeLanguage(newLanguage: string): void {
+    this.selectedLanguage = newLanguage;
+    this.translate.use(newLanguage);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
@@ -23,9 +30,18 @@ import { AppComponent } from './app.component';
     MatIconModule,
     MatMenuModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
