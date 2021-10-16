77d3882e4a813cd333ad9633959e7c41bc8fc23c
Christian Fritz <christian.fritz@dreamix.eu>
2021-10-16 17:51:28 +0300

    internationalization with i18next

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeBg from '@angular/common/locales/bg';
+import localeEn from '@angular/common/locales/en';
 
+import { I18NEXT_SERVICE, ITranslationService } from 'angular-i18next';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +13,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  public selectedLanguage: string = 'en';
+  public languages = ['bg', 'en']; 
+
+  constructor( @Inject(I18NEXT_SERVICE) private i18NextService: ITranslationService) { 
+    registerLocaleData(localeBg, 'bg');
+    registerLocaleData(localeEn, 'en');
+  }
+
+  public changeLanguage(newLanguage: string): void {
+    this.selectedLanguage = newLanguage;
+    this.i18NextService.changeLanguage(newLanguage);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,47 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { I18NextModule, I18NEXT_SERVICE, ITranslationService } from 'angular-i18next';
+import Backend from 'i18next-http-backend';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+export function appInit(i18next: ITranslationService) {
+  return () => i18next.use(Backend).init({
+    supportedLngs: ['en', 'bg'],
+    fallbackLng: 'en',
+    debug: true,
+    returnEmptyString: false,
+    backend: {
+      loadPath: 'assets/i18n/{{lng}}.json'
+    },
+  });
+}
+
+export function localeIdFactory(i18next: ITranslationService)  {
+  return i18next.language;
+}
+
+export const I18N_PROVIDERS = [
+{
+  provide: APP_INITIALIZER,
+  useFactory: appInit,
+  deps: [I18NEXT_SERVICE],
+  multi: true
+},
+{
+  provide: LOCALE_ID,
+  deps: [I18NEXT_SERVICE],
+  useFactory: localeIdFactory
+}];
 
 @NgModule({
   declarations: [
@@ -23,9 +55,11 @@ import { AppComponent } from './app.component';
     MatIconModule,
     MatMenuModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    I18NextModule.forRoot(),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient, I18N_PROVIDERS],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
